b60f1228663965d54f4a871bbfc56ce2a420f6e7
Aaron <ewass04@sky.com>
2024-11-20 08:04:23 +0000

    Major change
    Changed how the algorithm searches for spaces to be able to have multiple paths + style fixes

--- Program 2/Mouse Maze 2/Mouse Maze 2/Program.cs
+++ Program 2/Mouse Maze 2/Mouse Maze 2/Program.cs
@@ -9,9 +9,8 @@ namespace MouseMaze
     {
         static void Main(string[] args)
         {
-            
             // Defining the maze
-            char[,] Maze =
+            char[,] maze =
             {
                 {'0','1','1','1','1','1','1','1','1','1','1','1'},
                 {'1','0','1','1','0','1','1','0','0','1','0','1'},
@@ -21,21 +20,19 @@ namespace MouseMaze
                 {'1','1','1','1','1','0','1','1','0','1','0','1'},
                 {'1','0','1','0','0','1','1','0','1','1','0','1'},
                 {'1','1','0','1','1','1','1','0','1','0','1','1'},
-                {'1','0','1','0','0','0','1','1','1','1','1','1'},
-                {'1','0','1','1','1','1','0','1','1','0','1','1'},
-                {'1','1','1','0','1','1','0','1','0','1','0','1'},
+                {'1','0','1','0','0','0','1','1','1','1','0','1'},
+                {'1','0','1','1','1','1','0','1','1','1','0','1'},
+                {'1','1','1','0','1','1','0','1','0','0','0','1'},
                 {'1','0','0','1','0','0','1','0','1','1','1','E'},
-                {'1','1','1','1','1','1','1','1','1','1','1','1'},
+                {'1','1','1','1','1','1','1','1','1','1','1','1'}
             };
-
             
-            /*
-            Second test maze 
-            char[,] Maze =
+            /*Second test maze 
+            char[,] maze =
             {
                 {'0','1','1','1','1','1','1','1','1','1','1','1','1'},
-                {'1','0','1','1','0','1','0','1','1','0','0','0','E'},
-                {'1','1','0','1','0','1','0','1','0','1','1','1','1'},
+                {'1','0','1','1','0','1','1','0','0','0','0','0','E'},
+                {'1','1','0','1','0','1','0','1','1','0','1','1','1'},
                 {'1','1','1','0','1','0','1','1','1','0','0','0','1'},
                 {'1','0','1','0','1','0','1','1','0','1','1','1','1'},
                 {'1','0','1','0','1','1','0','1','1','0','0','1','1'},
@@ -50,75 +47,178 @@ namespace MouseMaze
                 {'1','0','1','0','0','1','1','0','1','1','0','1','1'},
                 {'1','0','1','1','1','0','0','1','0','0','1','0','1'},
                 {'1','1','1','1','1','1','1','1','1','1','1','1','1'}
-            };
-            
-            */
+            }; */
 
-            PrintMaze(Maze);
+            bool[,] visited = new bool[maze.GetLength(0), maze.GetLength(1)]; // 2D array to track if a spot has been visted
+            int timesExplored = 1; // Times rat has run around maze counter
+            bool endFound;
+            Stack<point> moves = new Stack<point>(); // Track of moves
+            Stack<point> routeToExit = new Stack<point>(); 
+            List<string> allRoutes = new List<string>(); // List of all paths to exit
+            string routeTaken = null;
+   
+            var position = new point { X = 0, Y = 0, count = 1, positionChecking = timesExplored }; // Starting position
 
-            bool endFound = false;
-
-            Stack<point> Moves = new Stack<point>(); // Track of moves
-            Stack<point> LastVisted = new Stack<point>(); // Track of moves before last
-
-            var position = new point { X = 0, Y = 0, Count = 1 }; // Starting position
-
-            do // Do until end is found or no other possible moves
+            do
             {
+                // Reset
+                maze[0, 0] = '0';
+                PrintMaze(maze, timesExplored);
+                moves.Clear();
+                routeToExit.Clear();
+                position = new point { X = 0, Y = 0, count = 1, positionChecking = timesExplored };
+                endFound = false;
 
-                if (Maze[position.X, position.Y] != 'E') position = FindNextMove(position, Maze, Moves, LastVisted);
-                else
+                do // Main exploration
                 {
-                    endFound = true;
+                    if (maze[position.X, position.Y] != 'E')
+                    {
+                        position = FindNextMove(position, maze, moves, timesExplored, visited);
+                    }
+                    else
+                    {
+                        endFound = true;
 
-                    Maze[position.X, position.Y] = 'R';
-                    PrintMaze(Maze);
+                        // Display rat
+                        maze[position.X, position.Y] = 'R';
+                        PrintMaze(maze, timesExplored);
+                        maze[position.X, position.Y] = position.X == 0 && position.Y == 0 ? '1' : '0'; // Reset
+                    }
 
-                    // Restore the original position in the maze.
-                    Maze[position.X, position.Y] = position.X == 0 && position.Y == 0 ? '1' : '0';
+                } while (moves.Count != 0 && !endFound); // Do while theres possible moves and exit hasnt been found
+
+                // Reset visited array and increase times explored
+                ResetVisitedArray(visited);
+                timesExplored++;
+
+                if (endFound)
+                {
+                    routeTaken = null;
+                    maze[position.X, position.Y] = 'E'; 
+                    Console.Write("End Found\n");
+
+                    routeTaken = BuildRouteToExit(moves, routeToExit);
+                    if (!allRoutes.Contains(routeTaken)) allRoutes.Add(routeTaken);
                 }
 
-            } while (Moves.Count != 0 && !endFound);
+            } while (endFound && timesExplored < 9);
 
-            if (endFound) 
+            if (endFound)
             {
                 Console.WriteLine("\nExit has been found");
-                Console.WriteLine("\nPath to exit:");
-                Console.WriteLine("-------------");
-
-                Stack<point> RouteToExit = new Stack<point>(); 
-
-                foreach (point p in Moves) RouteToExit.Push(p); // Get moves in correct order
-
-                // Display route to exit
-                foreach (point p in RouteToExit)
-                {
-                    Console.Write("(" + (p.Y + 1) + "," + (p.X +1) + ")");
-                    Console.Write(" --> ");
-                }
-
-                Console.Write("End Found\n");
+                DisplayRoutes(allRoutes);
             }
             else Console.WriteLine("\nThere is no exit");
         }
 
-        static void PrintMaze(char[,] maze)
+        public class point
         {
-            //Thread.Sleep(1000); // Delay so user can view maze
+            public int X { get; set; }
+            public int Y { get; set; }
+            public int count { get; set; }
+            public int positionChecking { get; set; }
+        }
+
+        static point FindNextMove(point position, char[,] maze, Stack<point> moves, int timesExplored, bool[,] visitedPositions)
+        {
+            bool pathFound = false;
+
+            // Get positions info
+            int x = position.X;
+            int y = position.Y;
+            int count = position.count;
+            int positionChecking = position.positionChecking;
+
+            var nextMove = new point { X = 1, Y = 1, count = 1 };
+
+            // Wrap around back to start
+            if (positionChecking != 1) positionChecking++;
+            else if (positionChecking == 9) positionChecking = 1;
+
+            // Define the directions array.
+            var directions = new (int dx, int dy)[]
+            {
+                (0, -1),   // North
+                (1, -1),   // North-East
+                (1, 0),    // East
+                (1, 1),    // South-East
+                (0, 1),    // South
+                (-1, 1),   // South-West
+                (-1, 0),   // West
+                (-1, -1)   // North-West
+            };
+
+            // For each possible direction, check for moves
+            for (int i = count; count <= 8; i++)
+            {
+                if (positionChecking == 9) positionChecking = 1;
+
+                // Get the corresponding co-ordinates based on counter
+                int dx = directions[positionChecking - 1].dx;
+                int dy = directions[positionChecking - 1].dy;
+
+                // Calculate the new position 
+                int newX = x + dx;
+                int newY = y + dy;
+
+                if (newX >= 0 && newY >= 0 && newX < maze.GetLength(0) && newY < maze.GetLength(1)) // Ensure in bounds
+                {
+                    char symbol = maze[newX, newY];
+
+                    // Check if it's a valid path and hasn't been visited before
+                    if ((symbol == '0' || symbol == 'E') && !visitedPositions[newX, newY])
+                    {
+                        pathFound = true;
+
+                        var currentPosition = new point { X = x, Y = y, count = count, positionChecking = positionChecking }; // Get current position
+                        moves.Push(currentPosition);
+
+                        // Mark this new position as visited
+                        visitedPositions[newX, newY] = true;
+
+                        nextMove = new point { X = newX, Y = newY, count = 1, positionChecking = position.positionChecking };
+                        break;
+                    }
+                }
+
+                count++;
+                positionChecking++;
+            }
+
+            // If no path found, go back to the previous space
+            if (!pathFound && moves.Count > 0) nextMove = moves.Pop();
+
+            // Show rat's current location in the maze
+            maze[position.X, position.Y] = 'R';
+            PrintMaze(maze, timesExplored);
+
+            // Restore the original position in the maze
+            maze[position.X, position.Y] = position.X == 0 && position.Y == 0 ? '1' : '0';
+
+            return nextMove;
+        }
+
+        static void PrintMaze(char[,] maze, int timesExplored)
+        {
+            Thread.Sleep(100); // Delay so user can view maze
             Console.Clear(); // Clear old maze
 
             Console.WriteLine("        Rat Maze        ");
             Console.WriteLine("------------------------\n");
 
+            Console.WriteLine("Times searched maze: " + timesExplored + "\n");
+
             for (int i = 0; i < maze.GetLength(0); i++)
             {
                 for (int j = 0; j < maze.GetLength(1); j++)
                 {
-                    // Change colour based upon whats being printed
-                    if (maze[i, j] == 'R') Console.ForegroundColor = ConsoleColor.Cyan;
-                    else if (maze[i, j] == '1') Console.ForegroundColor = ConsoleColor.Gray;
-                    else if (maze[i, j] == '0') Console.ForegroundColor = ConsoleColor.DarkGreen;
-                    else if (maze[i, j] == 'E') Console.ForegroundColor = ConsoleColor.Red;
+                    switch(maze[i, j]) // Set colour of character
+                    {
+                        case 'R': Console.ForegroundColor = ConsoleColor.Cyan; break;
+                        case '1': Console.ForegroundColor = ConsoleColor.Gray; break;
+                        case '0': Console.ForegroundColor = ConsoleColor.DarkGreen; break;
+                        case 'E': Console.ForegroundColor = ConsoleColor.Red; break;
+                    }
 
                     Console.Write(maze[i, j] + " ");
 
@@ -128,89 +228,46 @@ namespace MouseMaze
             }
         }
 
-        public class point
+        static string BuildRouteToExit(Stack<point> moves, Stack<point> routeToExit)
         {
-            public int X { get; set; }
-            public int Y { get; set; }
-            public int Count { get; set; }
+            string routeTaken = null;
 
+            foreach (point p in moves) routeToExit.Push(p); // Get moves in correct order
+            foreach (point p in routeToExit) routeTaken += "(" + (p.Y + 1) + "," + (p.X + 1) + ")" + " --> "; // Display route to exit
+
+            routeTaken += "End";
+
+            return routeTaken;
         }
 
-        static point FindNextMove(point position, char[,] Maze, Stack<point> Moves, Stack<point> LastVisted)
+        static void DisplayRoutes(List<string> allRoutes)
         {
-            bool pathFound = false;
+            Console.WriteLine("\nPaths to exit:");
+            Console.WriteLine("-------------");
 
-            int x = position.X;
-            int y = position.Y;
-            int count = position.Count;
+            string shortestRoute = null;
 
-            var nextMove = new point { X = 1, Y = 1, Count = 1 };
-
-            if (count != 1) count++;
-
-            // Define the directions array.
-            var directions = new (int dx, int dy)[]
+            foreach (string r in allRoutes)
             {
-                (0, -1),   // North
-                (1, 0),    // East
-                (0, 1),    // South
-                (-1, 0),   // West
-                (-1, 1),   // South-West
-                (-1, -1),  // North-West
-                (1, 1),    // South-East
-                (1, -1)    // North-East
-            };
+                Console.WriteLine(r + "\n\n"); // Display path
 
-            // For each possible direction, check for moves
-            for (int i = count; i <= 8; i++)  
+                if (shortestRoute == null || r.Length < shortestRoute.Length) shortestRoute = r; // Find which is the chortest path based on length of string
+            }
+
+            Console.WriteLine("Shortest path to the exit:");
+            Console.WriteLine("--------------------------\n\n");
+            Console.WriteLine(shortestRoute);
+        }
+        static void ResetVisitedArray(bool[,] visited)
+        {
+            for (int i = 0; i < visited.GetLength(0); i++)
             {
-                
-                // Get the corresponding co-oridents dependant on counter
-                int dx = directions[count - 1].dx;
-                int dy = directions[count - 1].dy;
-
-                // Calculate the new position 
-                int newX = x + dx;
-                int newY = y + dy;
-
-                if (newX >= 0 && newY >= 0) // if co-ordients are in the bounds of maze
+                for (int j = 0; j < visited.GetLength(1); j++)
                 {
-                    char symbol = Maze[newX, newY];
-
-                    // If it is a path and isnt the position came from 
-                    if ((symbol == '0' || symbol == 'E') && (Moves.Count == 0 || (newX != Moves.Peek().X || newY != Moves.Peek().Y)) && (LastVisted.Count == 0 || (newX != LastVisted.Peek().X || newY != LastVisted.Peek().Y)))
-                    {
-                        pathFound = true;
-
-                        var currentPosition = new point { X = x, Y = y, Count = i }; // get current position
-                        Moves.Push(currentPosition);
-                         
-                        nextMove = new point { X = newX, Y = newY, Count = 1 };
-                        break;
-                    }
+                    visited[i, j] = false;
                 }
-
-                count++;
-               
             }
-
-            // If no path go back to previous space
-            if (!pathFound && Moves.Count > 0)
-            {
-                nextMove = Moves.Pop();
-            }
-
-            // Show rat's current location in the maze
-            Maze[position.X, position.Y] = 'R';
-            PrintMaze(Maze);
-
-            Maze[position.X, position.Y] = position.X == 0 && position.Y == 0 ? '1' : '0';
-
-            var pastMove = new point { X = x, Y = y, Count = 1 };
-            LastVisted.Push(pastMove);
-
-            return nextMove;
         }
-
     }
 }
+    
